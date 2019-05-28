7a7c08ab41c5dc06e3ca22c2f9b4d51e63eb6c6a
Daniel <danielstarling4@gmail.com>
2019-05-28 13:21:47 -0700

    functions.py complete

--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -2,8 +2,8 @@
 Dictionaries are Python's implementation of associative arrays.
 There's not much different with Python's version compared to what
 you'll find in other languages (though you can also initialize and
-populate dictionaries using comprehensions just like you can with 
-lists!). 
+populate dictionaries using comprehensions just like you can with
+lists!).
 
 The docs can be found here:
 https://docs.python.org/3/tutorial/datastructures.html#dictionaries
@@ -55,4 +55,5 @@ for i in waypoints:
     print(i["name"])
 
     for i in waypoints:
-        sentence = "%s is located at %d latitude and %d longitude." % (i["name"], i["lat"], i["lon"])
+        sentence = "%s is located at %d latitude and %d longitude." % (
+            i["name"], i["lat"], i["lon"])
